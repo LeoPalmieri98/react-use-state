6d42f005fdae1a61084fd688b901cbc5f5efed2a
leopii98 <rippergames98@gmail.com>
2026-01-17 11:01:36 +0100

    add-.map

--- src/components/MyAccordion.jsx
+++ src/components/MyAccordion.jsx
@@ -1,9 +1,26 @@
 import { useState } from "react"
+import languages from "../components/languages.js"
 
 function MyAccordion() {
     return (
-        <button className="btn btn-primary">HTML</button>
-    )
-}
+        <div className="container mt-5">
+            {languages.map(language => {
+                return (
+                    <div key={language.id} className="mb-4">
+                        <button className="btn btn-primary mb-2">
+                            {language.title}
+                        </button>
 
+                        <div className="card">
+                            <h5 className="card-header card-title"></h5>
+                            <div className="card-body">
+                                <p className="card-text"></p>
+                            </div>
+                        </div>
+                    </div>
+                );
+            })}
+        </div>
+    );
+}
 export default MyAccordion
