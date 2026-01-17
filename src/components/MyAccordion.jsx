97ee1a78fcb296aefccdb2db8f7e6fd2b5120752
leopii98 <rippergames98@gmail.com>
2026-01-17 11:18:49 +0100

    fix-d-flex

--- src/components/MyAccordion.jsx
+++ src/components/MyAccordion.jsx
@@ -2,25 +2,30 @@ import { useState } from "react"
 import languages from "../components/languages.js"
 
 function MyAccordion() {
+
+
     return (
         <div className="container mt-5">
-            {languages.map(language => {
-                return (
-                    <div key={language.id} className="mb-4">
-                        <button className="btn btn-primary mb-2">
-                            {language.title}
-                        </button>
+            <div className="d-flex flex-wrap gap-4 mb-4">
+                {languages.map(language => (
+                    <button
+                        key={language.id}
+                        className="btn btn-primary"
+                    >
+                        {language.title}
+                    </button>
+                ))}
+            </div>
 
-                        <div className="card">
-                            <h5 className="card-header card-title"></h5>
-                            <div className="card-body">
-                                <p className="card-text"></p>
-                            </div>
-                        </div>
-                    </div>
-                );
-            })}
-        </div>
+            <div className="card">
+                <h5 className="card-header card-title"></h5>
+                <div className="card-body">
+                    <p className="card-text"></p>
+                </div>
+            </div >
+        </div >
+
     );
 }
-export default MyAccordion
+
+export default MyAccordion;
