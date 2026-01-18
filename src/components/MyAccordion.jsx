352c2bd5b8eafd6ef6f517cdaa665560760f9682
leopii98 <rippergames98@gmail.com>
2026-01-18 10:26:07 +0100

    complete.accordion-done

--- src/components/MyAccordion.jsx
+++ src/components/MyAccordion.jsx
@@ -3,28 +3,36 @@ import languages from "../components/languages.js"
 
 function MyAccordion() {
 
+    const [selectedLanguage, setSelectedLanguage] = useState(0);
 
+    function toggleMyAccordion(event) {
+        console.log(event)
+        setSelectedLanguage(event);
+    }
     return (
         <div className="container mt-5">
             <div className="d-flex flex-wrap gap-4 mb-4">
-                {languages.map(language => (
-                    <button
-                        key={language.id}
-                        className="btn btn-primary"
-                    >
+                {languages.map((language, index) => (
+                    <button key={language.id} onClick={() => { toggleMyAccordion(index) }}
+                        className={`btn ${selectedLanguage === index ? "btn-warning" : "btn-primary"}`}>
                         {language.title}
                     </button>
                 ))}
             </div>
 
-            <div className="card">
-                <h5 className="card-header card-title"></h5>
-                <div className="card-body">
-                    <p className="card-text"></p>
+            {selectedLanguage !== null && (
+                <div className="card">
+                    <h5 className="card-header card-title">
+                        {languages[selectedLanguage].title}
+                    </h5>
+                    <div className="card-body">
+                        <p className="card-text">
+                            {languages[selectedLanguage].description}
+                        </p>
+                    </div>
                 </div>
-            </div >
-        </div >
-
+            )}
+        </div>
     );
 }
 
